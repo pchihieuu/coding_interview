ff7015714b58d52574c9f4ef3242a120bdff73ca
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-31 15:56:56 +0700

    Add files via upload

---
+++ twoSum2.go
@@ -0,0 +1,16 @@
+package codinginterview
+
+func twoSum(numbers []int, target int) []int {
+	i := 0
+	j := len(numbers) - 1
+	for i < j {
+		if numbers[i]+numbers[j] == target {
+			return []int{i + 1, j + 1}
+		} else if numbers[i]+numbers[j] < target {
+			i++
+		} else {
+			j--
+		}
+	}
+	return []int{1, 1}
+}
