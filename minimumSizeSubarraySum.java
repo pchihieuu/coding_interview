4ef861d5e0e5f112609813c32f32dfdab833c9c7
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-31 16:10:55 +0700

    Add files via upload

---
+++ minimumSizeSubarraySum.java
@@ -0,0 +1,16 @@
+public class minimumSizeSubarraySum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int ans = Integer.MAX_VALUE;
+        int sum = 0;
+
+        for (int l = 0, r = 0; r < nums.length; ++r) {
+            sum += nums[r];
+            while (sum >= target) {
+                ans = Math.min(ans, r - l + 1);
+                sum -= nums[l++];
+            }
+        }
+
+        return ans != Integer.MAX_VALUE ? ans : 0;
+        }
+}
