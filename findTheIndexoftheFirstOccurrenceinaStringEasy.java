dc9b0a42627c26b7fc505d2db75b514c63cf5d18
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-31 15:50:57 +0700

    Add files via upload

---
+++ findTheIndexoftheFirstOccurrenceinaStringEasy.java
@@ -0,0 +1,12 @@
+public class findTheIndexoftheFirstOccurrenceinaStringEasy {
+    public int strStr(String haystack, String needle) {
+        final int m = haystack.length();
+        final int n = needle.length();
+
+        for (int i = 0; i < m - n + 1; ++i)
+            if (haystack.substring(i, i + n).equals(needle))
+                return i;
+
+        return -1;
+    }
+}
