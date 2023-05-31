efd87dbf3dd4e131c7c1d5f17a473db38e1df43f
Phan Chí Hiếu <98179861+Phanhieu1221@users.noreply.github.com>
2023-05-31 15:53:58 +0700

    Add files via upload

---
+++ validPalindrome.go
@@ -0,0 +1,31 @@
+package codinginterview
+
+func isPalindrome(s string) bool {
+	left := 0
+	right := len(s) - 1
+	for left < right {
+		if !isAlphanumeric(s[left]) {
+			left++
+			continue
+		}
+		if !isAlphanumeric(s[right]) {
+			right--
+			continue
+		}
+		if upperCase(s[left]) != upperCase(s[right]) {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+func isAlphanumeric(char byte) bool {
+	return (char >= '0' && char <= '9') || (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z')
+}
+func upperCase(char byte) byte {
+	if char >= 'a' && char <= 'z' {
+		return char - 32
+	}
+	return char
+}
